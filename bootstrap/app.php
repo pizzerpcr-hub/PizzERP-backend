e08321aaad351fa42508ba4dd9abd3fecbd3002e
Anderson Chacón <andersonchacon360@gmail.com>
2026-09-20 22:44:18 -0600

    feat: ruta api Creacion Usuario

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         commands: __DIR__.'/../routes/console.php',
+        api: __DIR__.'/../routes/api.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
